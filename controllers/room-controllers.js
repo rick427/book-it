c5fdd16231d691a8f7a91a0c08bb9b05ed0463f8
rick427 <rchard@yahoo.com>
2021-05-21 15:36:07 +0100

    added controller for new room

--- controllers/room-controllers.js
+++ controllers/room-controllers.js
@@ -1,7 +1,20 @@
 import Room from '@/models/room';
 
-const allRooms = (req, res) => {
-    res.status(200).json({success: true, message: 'All Rooms'})
+const allRooms = async (_, res) => {
+    try {
+        const rooms = await Room.find();
+    
+        res.status(200).json({
+            success: true,
+            count: rooms.length,
+            data: rooms
+        });
+    } catch (error) {
+        res.status(400).json({
+            success: false,
+            error: error.message
+        })
+    }
 }
 
 //@ Create new room  --> /api/rooms
